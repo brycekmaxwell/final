26fad1175bbbd106e103855b555c2493d3a25c95
Bryce Maxwell <brycemaxwell@Bryces-MBP.hitronhub.home>
2023-01-16 19:34:21 -0600

    updating folders

---
+++ flask_app/controllers/controllers_users.py
@@ -0,0 +1,47 @@
+from flask_app import app
+from flask import Flask, render_template, redirect, request, session, flash
+from flask_app.models import models_users
+# from flask_app.models import model_show
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
+@app.route("/")
+def home():
+    return render_template("log_in.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+    valid_user = models_users.User.create_valid_user(request.form)
+    if not valid_user:
+        return redirect("/")
+    session["user_id"] = valid_user.id
+    return redirect("/dashboard")
+
+@app.route("/log_in", methods=["POST"])
+def login():
+    valid_user = models_users.User.authenticated_user_by_input(request.form)
+    if not valid_user:
+        flash("Invalid Email","login")
+        return redirect("/")
+    if not bcrypt.check_password_hash(valid_user.password, request.form["password"]):
+        flash("Invalid Password", "login")
+        return redirect("/")
+    session["user_id"]=valid_user.id
+    return redirect("/dashboard")
+
+@app.route("/dashboard")
+def dashboard():
+    if "user_id" not in session: 
+        return redirect("/logout")
+    user = models_users.User.get_by_id({"id":session["user_id"]})
+    shows = model_show.Show.get_user_shows({"id":session["user_id"]})
+    users = models_users.User.get_all()
+    return render_template("shows.html", user=user, users=users, shows=shows)
+
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+    
